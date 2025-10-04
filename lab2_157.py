af4a1d7dde93c317e54f527c950762dcbebd2c64
Stakhmich Karina <karinastahmic@gmail.com>
2025-10-04 13:25:16 +0300

    111

--- lab2_157.py
+++ lab2_157.py
@@ -11,6 +11,7 @@ result = new_whole + "." + new_fraction
 result_float = float(result)
 
 if int(new_fraction) == 0:
-    print("As a result, the fractional part became zero:", result_float)
+    print("As a result, the fractional part became zero:",\
+           result_float)
 else:
      print("New number: ", result_float)
