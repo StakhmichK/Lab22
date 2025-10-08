816431359bc1eefc0612dff9c2acdaf401c2eda4
Stakhmich Karina <karinastahmic@gmail.com>
2025-10-08 16:01:50 +0300

    111

--- lab2_176.py
+++ lab2_176.py
@@ -1,8 +1,28 @@
-n = (input("Enter 4-digit number: ")).strip()
-result = ""
-for digit in n:
-        if int(digit) % 2 == 0:
-                result += "*"
-        else:
-                result += digit
-print("Your number is: ", result)
+n = int(input("Enter 4-digit number: "))
+
+a = n // 1000         
+b = (n // 100) % 10    
+c = (n // 10) % 10     
+d = n % 10        
+
+if a % 2 == 0:
+    a = '*'
+else:
+    a = str(a)
+
+if b % 2 == 0:
+    b = '*'
+else:
+    b = str(b)
+
+if c % 2 == 0:
+    c = '*'
+else:
+    c = str(c)
+if d % 2 == 0:
+    d = '*'
+else:
+    d = str(d)
+
+result = a + b + c + d
+print("Result is: ", result)
